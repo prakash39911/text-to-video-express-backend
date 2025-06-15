52e0dba9ceea15745196516ce74ee705c12110a4
chandra prakash <prakash39911@gmail.com>
2025-06-15 17:09:53 +0530

    minor changes in entry file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { Response } from "express";
 import cors from "cors";
 import mergeRoute from "./routes/mergeRoute";
 
@@ -11,6 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/v1/api/mergeVideoAudio", mergeRoute);
+
+app.get("/", (res: Response) => {
+  Response.json({
+    message: "Hello! Your text-to-video server is running",
+  });
+  return;
+});
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Server Started On Port:-", process.env.PORT);
